ee0005593dca275966eaebde72579174e5d2b3fa
marco junior <marcojuniorengineer@gmail.com>
2022-03-22 08:34:08 -0300

    criação do repositório

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import App from "./componentes/App";
+
+import "./assets/style/reset.css";
+import "./assets/style/style.css";
+
+
+ReactDOM.render(<App />, document.querySelector(".root"));
